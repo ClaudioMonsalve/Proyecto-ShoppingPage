2a28774c36fa81e26b5e4b2366a2f8ddb5d12382
ClaudioMonsalv <claudiomonsalve1287@gmail.com>
2025-12-16 00:42:36 -0300

    pepe
    asdasd

--- Shopping_Mary/src/supabaseClient.js
+++ Shopping_Mary/src/supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Pon aquí tu URL y tu ANON KEY
-const SUPABASE_URL = 'https://mromohsaigquffgkzgny.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1yb21vaHNhaWdxdWZmZ2t6Z255Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NTI5MTksImV4cCI6MjA3NTQyODkxOX0.Kc1dlQqya4-6e8KJ2uhNw-fWh5pi5Fc_pTUc0EIKvpw'
+const SUPABASE_URL = 'https://oigjcdezysslhyuxfeew.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pZ2pjZGV6eXNzbGh5dXhmZWV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NTI5MTYsImV4cCI6MjA3NTQyODkxNn0.mz0guPPkGjE5AgcKlFcDIOyOjbSQy3Ta8zYXx95VmIw'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
